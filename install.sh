6a13e4488ab93cbaf9445cbef0539af93e32cb1d
Shousuke Komi <109201315+Komi7@users.noreply.github.com>
2022-11-08 10:03:18 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ pacman -S libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
 # Installing Essential Programs 
 pacman -S bspwm sxhkd rofi polybar picom thunar nitrogen unzip wget pulseaudio pavucontrol polkit-gnome alacritty sddm linux linux-headers neovim
 # Installing Other less important Programs
-pacman -S neofetch flameshot psmisc papirus-icon-theme mpv thunar-archive-plugin zsh zsh-syntax-highlighting pacman-contrib
+pacman -S neofetch flameshot psmisc papirus-icon-theme mpv thunar-archive-plugin zsh zsh-syntax-highlighting pacman-contrib feh
 
 
 
